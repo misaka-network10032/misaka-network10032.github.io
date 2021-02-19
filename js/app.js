eb8a96794df8d066a2c00e5f9cd668fa6e7869ac
misaka-network10032 <18791055956@163.com>
2021-02-19 15:52:46 +0800

    Site updated: 2021-02-19 15:52:46

--- js/app.js
+++ js/app.js
@@ -353,3 +353,24 @@ var Debounce = (fn, t) => {
     100);
   }
 })(jQuery);
+
+function colortag(){
+  $("ul.tag-list li").each(function(){
+      let random = Math.floor(Math.random()*5+1);
+      if(random == 2){
+          $(this).find('a').addClass('tagred');
+      } else if ( random == 3 ){
+          $(this).find('a').addClass('tagyellow');
+      } else if ( random == 4 ){
+          $(this).find('a').addClass('tagblue');
+      } else if ( random == 5 ){
+          $(this).find('a').addClass('taggreen');
+      } else if ( random == 2 ){
+          $(this).find('a').addClass('tagpurple');
+      } else if ( random == 1 ){
+          $(this).find('a').addClass('taggrown');
+      }
+   
+  });}
+   
+  colortag();
